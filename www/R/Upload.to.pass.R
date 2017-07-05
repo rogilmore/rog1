d7d8a8265843ebc5ad96715023c368d7d0e650da
rogilmore <thatrickgilmore@gmail.com>
2017-07-05 11:47:19 -0400

    Update with talks, Nat Hum Beh

---
+++ www/R/Upload.to.pass.R
@@ -0,0 +1,8 @@
+# Mount pass space first 
+# Command+K smb://smb.pass.psu.edu/pass/users/r/o/rog1
+# Need to script this...
+
+# Add error checking
+
+# Copy local www to mounted volume
+system(command = "cp -r www/ /Volumes/rog1/www ")
